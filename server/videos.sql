197ae2ea8b0c5b48221a58e2b65b706bd247735b
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-05-31 13:37:03 +0100

    Create videos.sql

---
+++ server/videos.sql
@@ -0,0 +1,4 @@
+CREATE TABLE videos (id SERIAL PRIMARY KEY,
+                                        title VARCHAR(2500) NOT NULL,
+                                            link VARCHAR(250) NOT NULL,
+                                                rating INTEGER NOT NULL );
