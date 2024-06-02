01e5fbd0628461a0b862283a0ad8f77ab84d739f
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-06-02 11:09:37 +0100

    Update videos.sql

--- server/videos.sql
+++ server/videos.sql
@@ -1,4 +1,8 @@
 CREATE TABLE videos (id SERIAL PRIMARY KEY,
                                         title VARCHAR(2500) NOT NULL,
-                                            link VARCHAR(250) NOT NULL,
+                                            url VARCHAR(250) NOT NULL,
                                                 rating INTEGER NOT NULL );
+
+
+INSERT INTO videos (title, url, rating)VALUES ('Book 100s of Appointments Every Single Month With Ideal Clients
+', 'https://www.youtube.com/watch?v=qzoq1NSIpzw', 1964);
