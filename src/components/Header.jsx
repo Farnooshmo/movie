abc5746fc4c2b4c58c59afc95e4f92590c93d9b3
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-06-04 18:07:23 +0100

    add a h4 to the header and inport the css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import "./Header.css"
 const Header = () => {
     return (
       <header className="app-header">
         <h1>Video Recommendation</h1>
+        <h4>Welcome to the platform. Add your favorite YouTube videos to discover and share the latest and greatest video clips. Don't forget to up-vote your favorites!</h4>
       </header>
     );
   };
