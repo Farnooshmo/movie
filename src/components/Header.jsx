4172a84117fed33305633f2bb50319d9618376d5
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-06-07 20:43:39 +0100

    add a h3 to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ const Header = () => {
     return (
       <header className="app-header">
         <h1>Video Recommendation</h1>
-        <h4>Welcome to the platform. Add your favorite YouTube videos to discover and share the latest and greatest video clips. Don't forget to up-vote your favorites!</h4>
+        <h3>Welcome to the platform.</h3> 
+        <h4>Add your favorite YouTube videos to discover and share the latest and greatest video clips. Don't forget to up-vote your favorites!</h4>
       </header>
     );
   };
